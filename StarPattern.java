cc95f18e9ade208187dbc24a6a6ead82e0f5e0b4
ratan01 <rudratan00@gmail.com>
2021-04-14 22:20:57 +0530

    Star Pattern

---
+++ StarPattern.java
@@ -0,0 +1,21 @@
+
+import java.util.*;
+public class StarPattern {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number ");
+		int n= sc.nextInt();
+		sc.close();
+		for(int i=1; i<=n; i++) {
+			for(int j=1; j<=n; j++) {
+				if(i==n/2||j==n/2||i+j==n+1||i==j) {
+					System.out.print("*");
+				}
+				else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+	}
+}
