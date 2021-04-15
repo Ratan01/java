5bf9e416c7b093c4221608bf515bce6bcfc619b5
ratan01 <rudratan00@gmail.com>
2021-04-15 19:00:53 +0530

    Pattern 12

---
+++ Pattern_12.java
@@ -0,0 +1,17 @@
+
+import java.util.*;
+public class Pattern_2 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number ");
+		int n= sc.nextInt();
+		sc.close();
+		for(int i=1; i<=n; i++) {
+			for(int j=i; j<=n; j++) {
+				System.out.print(j);
+			}
+			System.out.println();
+		}
+	}
+
+}
