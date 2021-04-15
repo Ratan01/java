6cb5bca8cf329da5ceffceb0dea8f0b101c7aaad
ratan01 <rudratan00@gmail.com>
2021-04-15 19:04:02 +0530

    Pattern 17

---
+++ Pattern_17.java
@@ -0,0 +1,19 @@
+
+import java.util.*;
+public class Pattern_7 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number ");
+		int n= sc.nextInt();
+		sc.close();
+		for(int i=n; i>=1; i--) {
+			for(int j=1; j<=n-i; j++) {
+				System.out.print(" ");
+			}
+			for(int k=1; k<=2*i-1; k++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
