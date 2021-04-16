ff1bfcc3bdd2bb47670300e21a1018709e70ba9e
ratan01 <rudratan00@gmail.com>
2021-04-16 23:03:59 +0530

    Pattern 23

---
+++ Pattern_23.java
@@ -0,0 +1,27 @@
+
+import java.util.*;
+public class Pattern_6 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number ");
+		int n=sc.nextInt();
+		sc.close();
+		for(int i=1; i<=n; i++) {
+			for(int j=1; j<=n-i; j++) {
+				System.out.print(" ");
+			}
+			int k=n-i+1;
+			for(int j=1; j<=2*i-1; j++) {
+				System.out.print(k+"");
+				if(j<i) {
+					k++;
+				}
+				else {
+					k--;
+				}
+			}
+			System.out.println();
+		}
+	}
+
+}
