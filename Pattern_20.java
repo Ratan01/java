25e3a2e37f513d4169e4557c82a11bb46e83d406
ratan01 <rudratan00@gmail.com>
2021-04-16 23:02:40 +0530

    Pattern 20

---
+++ Pattern_20.java
@@ -0,0 +1,26 @@
+
+import java.util.*;
+public class Pattern_3 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number ");
+		int n=sc.nextInt();
+		sc.close();
+		for(int i=1; i<=n; i++) {
+			for(int j=1; j<=n-i; j++) {
+				System.out.print(" ");
+			}
+			int k=1;
+			for(int j=1; j<=2*i-1; j++) {
+				System.out.print(k+"");
+				if(j<i) {
+					k++;
+				}
+				else {
+					k--;
+				}
+			}
+			System.out.println();
+		}
+	}
+}
