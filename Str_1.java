aa5caac27616717e8ab7e44a73ca172c6411857b
ratan01 <rudratan00@gmail.com>
2021-04-19 23:21:23 +0530

    String Pattern 1

---
+++ Str_1.java
@@ -0,0 +1,18 @@
+package d01_04;
+import java.util.*;
+public class Str_1 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the name ");
+		String s= sc.next();
+		int n=s.length();
+		sc.close();
+		for(int i=0; i<=n; i++) {
+			for(int j=0; j<=i; j++) {
+				System.out.print(s.charAt(j)+" ");
+			}
+			System.out.println();
+		}
+	}
+
+}
