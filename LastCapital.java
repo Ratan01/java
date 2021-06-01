9e47b52729c1ec9d6829c3b11378ec8054a713ac
Rudra01 <rudratan00@gmail.com>
2021-06-01 18:44:35 +0530

    Every word last char in Capital Letter

---
+++ LastCapital.java
@@ -0,0 +1,23 @@
+
+import java.util.*;
+public class LastCapital {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the string: ");
+		String st=sc.nextLine();
+		char ch[]=st.toCharArray();
+		for(int i=0; i<ch.length; i++) {
+			if(i==ch.length-1 && ch[i]!=' ' || ch[i]!=' ' && ch[i+1]==' ') {
+				if(ch[i]>='a'&&ch[i]<='z') {
+					ch[i]=(char)(ch[i]-32);
+				}
+				else if(ch[i]>='A'&&ch[i]<='Z') {
+					ch[i]=(char)(ch[i]+32);
+				}
+			}
+		}
+		st=new String(ch);
+		System.out.println(st);
+	}
+
+}
